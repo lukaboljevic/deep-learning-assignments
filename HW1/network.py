0064d6024b5cbd09b63539c071a64a1817117b9e
lukaboljevic <lukaboljevic.boljevic@gmail.com>
2023-03-12 19:33:53 +0100

    Implement Adam, fix bugs with L2, other additions
    Other additions include slightly more readable code, more doc comments, plotting epoch losses and CAs on validation set on different figures, etc.

--- HW1/network.py
+++ HW1/network.py
@@ -14,7 +14,7 @@ class Network(object):
     Notations taken from lecture slides, i.e. from http://neuralnetworksanddeeplearning.com/
     """
 
-    def __init__(self, sizes, optimizer="sgd"):
+    def __init__(self, sizes, optimizer="mbgd", eps=1e-8):
         """
         Initialize the weights and biases for the neural network, and optionally the buffers for Adam.
 
@@ -43,8 +43,11 @@ class Network(object):
         self.biases = [np.zeros((x, 1)) for x in sizes[1:]]
         self.optimizer = optimizer
         if self.optimizer == "adam":
-            # TODO Implement the buffers necessary for the Adam optimizer.
-            pass
+            self.m_weights = [np.zeros_like(w) for w in self.weights]
+            self.v_weights = [np.zeros_like(w) for w in self.weights]
+            self.m_biases = [np.zeros_like(b) for b in self.biases]
+            self.v_biases = [np.zeros_like(b) for b in self.biases]
+            self.eps = eps
 
     
     def forward_pass(self, input):
@@ -110,17 +113,17 @@ class Network(object):
                      of the m elements. Activations are calculated during forward pass.
 
         Returns:
-            gradients_w - array of length num_layers-1 (same as self.weights), containing arrays of
-                          shape [n_L+1, n_L] - same shape as weights for layer L in self.weights.
-                          Each element of gradients_w contains the gradient of the loss function
-                          w.r.t. the weights (i.e. the partial derivative of the loss function
-                          w.r.t. all of the individual weights).
+            grad_weights - array of length num_layers-1 (same as self.weights), containing arrays of
+                           shape [n_L+1, n_L] - same shape as weights for layer L in self.weights.
+                           Each element of grad_weights contains the gradient of the loss function
+                           w.r.t. the weights (i.e. the partial derivative of the loss function
+                           w.r.t. all of the individual weights).
 
-            gradients_b - array of length num_layers-1 (same as self.biases), containing arrays of
-                          shape [n_L+1, 1] - same shape as biases for layer L in self.biases.
-                          Each element of gradients_b contains the gradient of the loss function
-                          w.r.t. the biases (i.e. the partial derivative of the loss function
-                          w.r.t. all of the individual biases).
+            grad_biases  - array of length num_layers-1 (same as self.biases), containing arrays of
+                           shape [n_L+1, 1] - same shape as biases for layer L in self.biases.
+                           Each element of grad_biases contains the gradient of the loss function
+                           w.r.t. the biases (i.e. the partial derivative of the loss function
+                           w.r.t. all of the individual biases).
 
         Further info:
             The indexing used here is not exactly the same as the indexing used in lecture slides, 
@@ -130,8 +133,9 @@ class Network(object):
             correct (and they 100% are).
         """
         batch_size = target.shape[1]
-        gradients_w = [np.zeros(w.shape) for w in self.weights]
-        gradients_b = [np.zeros(b.shape) for b in self.biases]
+        grad_weights = [np.zeros(w.shape) for w in self.weights]
+        grad_biases = [np.zeros(b.shape) for b in self.biases]
+
 
         # Backpropagation of the error through the layers
         num_weights = len(self.weights)
@@ -141,42 +145,70 @@ class Network(object):
             else:
                 delta = np.dot(np.transpose(self.weights[l+1]), delta) * sigmoid_prime(Zs[l])
                 
-            gradients_w[l] = np.dot(delta, np.transpose(As[l])) / batch_size
-            gradients_b[l] = np.sum(delta, axis=1, keepdims=True) / batch_size  # not axis=0!
+            grad_weights[l] = np.dot(delta, np.transpose(As[l])) / batch_size
+            grad_biases[l] = np.sum(delta, axis=1, keepdims=True) / batch_size  # not axis=0!
         
-        return gradients_w, gradients_b
+        return grad_weights, grad_biases
 
 
-    def update_network(self, gradients_w, gradients_b, eta):
+    def update_network(self, grad_weights, grad_biases, eta, t, lmbd=0.0, n=None):
         """
-        Update the weights and biases based on the used optimizer (SGD or Adam).
+        Update the weights and biases based on the used optimizer (MBGD or Adam).
 
         Parameters:
-            gradients_w - array of length num_layers-1 (same as self.weights), containing arrays of
-                          shape [n_L+1, n_L] - same shape as weights for layer L in self.weights.
-                          Each element of gradients_w contains the gradient of the loss function
-                          w.r.t. the weights (i.e. the partial derivative of the loss function
-                          w.r.t. all of the individual weights). These gradients are calculated
-                          during backward pass.
+            grad_weights - array of length num_layers-1 (same as self.weights), containing arrays of
+                           shape [n_L+1, n_L] - same shape as weights for layer L in self.weights.
+                           Each element of grad_weights contains the gradient of the loss function
+                           w.r.t. the weights (i.e. the partial derivative of the loss function
+                           w.r.t. all of the individual weights). These gradients are calculated
+                           during backward pass.
 
-            gradients_b - array of length num_layers-1 (same as self.biases), containing arrays of
-                          shape [n_L+1, 1] - same shape as biases for layer L in self.biases.
-                          Each element of gradients_b contains the gradient of the loss function
-                          w.r.t. the biases (i.e. the partial derivative of the loss function
-                          w.r.t. all of the individual biases). These gradients are calculated
-                          during backward pass.
+            grad_biases  - array of length num_layers-1 (same as self.biases), containing arrays of
+                           shape [n_L+1, 1] - same shape as biases for layer L in self.biases.
+                           Each element of grad_biases contains the gradient of the loss function
+                           w.r.t. the biases (i.e. the partial derivative of the loss function
+                           w.r.t. all of the individual biases). These gradients are calculated
+                           during backward pass.
 
             eta         - (current) learning rate
+
+            t           - current iteration, used for computing bias correction terms for Adam
+
+            lmbd        - L2 regularization parameter
+
+            n           - size of the TODO, used when calculating the weight decays
         """
 
-        # SGD
-        if self.optimizer == "sgd":
+        weight_decays = [0] * len(self.weights)
+        if lmbd != 0.0:
+            if not n:
+                raise ValueError("Size of training data (n) not given for L2 regularization when updating weights.")
             for i in range(len(self.weights)):
-                self.weights[i] = self.weights[i] - eta * gradients_w[i]
-                self.biases[i] = self.biases[i] - eta * gradients_b[i]
+                weight_decays[i] = (eta * lmbd) / n * self.weights[i]
+
+        if self.optimizer == "mbgd":
+            for i in range(len(self.weights)):
+                self.weights[i] = self.weights[i] - weight_decays[i] - eta * grad_weights[i]
+                self.biases[i] = self.biases[i] - eta * grad_biases[i]
+
         elif self.optimizer == "adam":
-            # TODO Implement the update function for Adam:
-            pass
+            for i in range(len(self.m_weights)):
+                self.m_weights[i] = beta1 * self.m_weights[i] + (1 - beta1) * grad_weights[i]
+                self.m_biases[i] = beta1 * self.m_biases[i] + (1 - beta1) * grad_biases[i]
+                
+                self.v_weights[i] = beta2 * self.v_weights[i] + (1 - beta2) * np.square(grad_weights[i])
+                self.v_biases[i] = beta2 * self.v_biases[i] + (1 - beta2) * np.square(grad_biases[i])
+
+                # Compute bias correction terms - makes learning slightly faster
+                # https://stats.stackexchange.com/questions/232741/why-is-it-important-to-include-a-bias-correction-term-for-the-adam-optimizer-for
+                # TODO ...
+
+                adam_weights = self.m_weights[i] / (np.sqrt(self.v_weights[i]) + self.eps)
+                adam_biases = self.m_biases[i] / (np.sqrt(self.v_biases[i]) + self.eps)
+
+                self.weights[i] = self.weights[i] - weight_decays[i] - eta * adam_weights
+                self.biases[i] = self.biases[i] - eta * adam_biases
+
         else:
             raise ValueError(f"Unknown optimizer: {self.optimizer}")
 
@@ -187,7 +219,8 @@ class Network(object):
               epochs,
               mini_batch_size,
               eta, lr_schedule="no",
-              **kwargs):
+              k=0.001,
+              lmbd=0.0):
         """
         Train the neural network.
 
@@ -208,6 +241,12 @@ class Network(object):
 
             eta             - learning rate
 
+            lr_schedule     - learning rate schedule, either "no" for no schedule, or "exponential"
+
+            k               - the decay rate of learning rate, only used when lr_schedule == "exponential"
+
+            lmbd            - L2 regularization parameter
+
         Returns:
             epoch_losses   - a list containing the (average) loss for each epoch
 
@@ -220,9 +259,6 @@ class Network(object):
         n = training_data.shape[1]
         epoch_losses = []
         validation_CAs = []
-        if lr_schedule == EXP_LR and not kwargs.get("k"):
-            raise ValueError(f"Decay rate k for exponential learning rate decay not provided.")
-        k = kwargs.get("k")
 
         for epoch in range(epochs):
             print()
@@ -235,14 +271,14 @@ class Network(object):
             num = len(mini_batches)
 
             for i, mini_batch in enumerate(mini_batches):
-                if (i+1) % 50 == 0: 
+                if (i+1) % 100 == 0: 
                     print(f"\tMini-batch {i+1} / {num}")
 
                 output, Zs, As = self.forward_pass(mini_batch[0])
-                gradients_w, gradients_b = \
+                grad_weights, grad_biases = \
                     self.backward_pass(output, mini_batch[1], Zs, As)
 
-                self.update_network(gradients_w, gradients_b, eta_current)
+                self.update_network(grad_weights, grad_biases, eta_current, iteration_index, lmbd=lmbd, n=mini_batch_size)
 
                 if lr_schedule == "no":
                     eta_current = eta
@@ -251,33 +287,38 @@ class Network(object):
 
                 iteration_index += 1
 
-                loss = cross_entropy(mini_batch[1], output)
+                loss = cross_entropy(mini_batch[1], output, lmbd=lmbd, n=mini_batch_size, weights=self.weights)
                 loss_avg += loss
 
             epoch_loss = loss_avg / len(mini_batches)
             epoch_losses.append(epoch_loss)
             print(f"Epoch {epoch + 1} complete")
             print(f"Loss: {epoch_loss}")
+
             if epoch == 0 or (epoch + 1) % 5 == 0:
-                ca = self.eval_network(val_data, val_class)
+                ca = self.eval_network(val_data, val_class, lmbd=lmbd, n=mini_batch_size)
                 validation_CAs.append((epoch, ca))
 
         return epoch_losses, validation_CAs
 
 
-    def eval_network(self, data, data_class):
+    def eval_network(self, data, data_class, lmbd=0.0, n=None):
         """
         Evaluate the (so far) trained network on the provided data and classes/labels.
 
         Parameters:
-            data - numpy array of dimensions [n0 x m], where m is the number of examples in 
-                   the data, and n0 is the number of input attributes. The data is coming
-                   either from the validation set, or test set.
+            data       - numpy array of dimensions [n0 x m], where m is the number of examples in 
+                         the data, and n0 is the number of input attributes. The data is coming
+                         either from the validation set, or test set.
 
             data_class - numpy array of dimensions [c x m], where c is the number of classes
+
+            lmbd       - L2 regularization parameter
+
+            n          - size of the TODO, used for regularization
         """
         
-        n = data.shape[1]
+        num_samples = data.shape[1]
         loss_avg = 0.0
         tp = 0.0
 
@@ -285,34 +326,16 @@ class Network(object):
             example = np.expand_dims(data[:, i], -1)  # one singular training example
             example_class = np.expand_dims(data_class[:, i], -1)
             example_class_num = np.argmax(data_class[:, i], axis=0)
-            output, _, _ = self.forward_pass(example)  # TODO maybe just give data[:, i] here, see what fits nicer
-            # output, _, _ = self.forward_pass(data[:, i])
+            output, _, _ = self.forward_pass(example)
             output_num = np.argmax(output, axis=0)[0]
             tp += int(example_class_num == output_num)
 
-            loss = cross_entropy(example_class, output)
+            loss = cross_entropy(example_class, output, lmbd=lmbd, n=n, weights=self.weights)
             loss_avg += loss
 
-        print(f"Validation Loss: {loss_avg / n}")
-        print(f"Classification accuracy: {tp / n}")
-        return tp / n
-
-
-def load_data_cifar(train_file, test_file):
-    """
-    Load the CIFAR10 dataset
-    """
-    train_dict = unpickle(train_file)
-    test_dict = unpickle(test_file)
-    train_data = np.array(train_dict['data']) / 255.0
-    train_class = np.array(train_dict['labels'])
-    train_class_one_hot = np.zeros((train_data.shape[0], 10))
-    train_class_one_hot[np.arange(train_class.shape[0]), train_class] = 1.0
-    test_data = np.array(test_dict['data']) / 255.0
-    test_class = np.array(test_dict['labels'])
-    test_class_one_hot = np.zeros((test_class.shape[0], 10))
-    test_class_one_hot[np.arange(test_class.shape[0]), test_class] = 1.0
-    return train_data.transpose(), train_class_one_hot.transpose(), test_data.transpose(), test_class_one_hot.transpose()
+        print(f"Validation Loss: {loss_avg / num_samples}")
+        print(f"Classification accuracy: {tp / num_samples}")
+        return tp / num_samples
 
 
 if __name__ == "__main__":
@@ -333,17 +356,20 @@ if __name__ == "__main__":
 
 
     # Hyperparameters
-    optimizer = "sgd"
-    hidden_layers = [200, 200, 200]
-    num_epochs = 20
-    mini_batch_size = 64
-    eta = 0.3
-    lr_schedule = EXP_LR
-    k = 0.001
-    lmbd = 15  # regularization parameter or None, if we don't want regularization
+    beta1           = 0.9  # beta1 for Adam optimizer
+    beta2           = 0.999  # beta2 for Adam optimizer
+    optimizer       = "adam"  # "mbgd" or "adam"
+    hidden_layers   = [500, 300, 100]  # structure of hidden layers
+    num_epochs      = 30
+    mini_batch_size = 32
+    eta             = 0.001  # learning rate; when optimizer == "adam", set to 0.001 by default
+    # lr_schedule     = EXP_LR
+    lr_schedule     = "no"
+    k               = 0.001
+    lmbd            = 0.1  # regularization parameter or 0.0, if we don't want regularization
 
 
-    # Train and evaluate
+    # Train
     net = Network([train_data.shape[0],  # input layer
                    *hidden_layers,
                    train_class.shape[0]],  # output layer
@@ -352,31 +378,45 @@ if __name__ == "__main__":
                                       num_epochs, mini_batch_size,
                                       eta, lr_schedule=lr_schedule,
                                       k=k, lmbd=lmbd)
+    
+
+    # Evaluate and print network settings
     print()
     print()
     print("=" * 50)
     print("=" * 50)
     net.eval_network(test_data, test_class)
     print()
-    print("=" * 50)
-    network_structure = f"\tOptimizer: \t\t{optimizer.upper()}\n" + \
-                        f"\tHidden layers: \t\t{hidden_layers}\n" + \
-                        f"\tNumber of epochs: \t{num_epochs}\n" + \
-                        f"\tMini-batch size: \t{mini_batch_size}\n" + \
-                        f"\tLearning rate: \t\t{eta}\n" + \
-                        f"\tLR schedule: \t\t{lr_schedule}\n"
+    network_settings = f"\tOptimizer: \t\t{optimizer.upper()}\n" + \
+                       f"\tHidden layers: \t\t{hidden_layers}\n" + \
+                       f"\tNumber of epochs: \t{num_epochs}\n" + \
+                       f"\tMini-batch size: \t{mini_batch_size}\n" + \
+                       f"\tLearning rate: \t\t{eta}\n" + \
+                       f"\tLR schedule: \t\t{lr_schedule}\n"
     if lr_schedule == EXP_LR:
-        network_structure += f"\tDecay rate k: \t\t{k}\n"
-    print(f"Network structure:\n{network_structure}")
+        network_settings += f"\tDecay rate k: \t\t{k}\n"
+    if lmbd != 0.0:
+        network_settings += f"\tLambda (for L2): \t{lmbd}\n"
+    if optimizer == "adam":
+        network_settings += f"\tBetas (Adam): \t\tbeta1={beta1}, beta2={beta2}\n"
+    print(f"Network settings:\n{network_settings}")
 
 
     # Plot the average loss of each epoch, and classification accuracies on the validation set
     val_epochs, ca_values = list(zip(*val_CAs))
     val_epochs, ca_values = list(val_epochs), list(ca_values)
     plt.style.use("ggplot")
-    plt.figure(figsize=(10, 7))
-    plt.plot(range(len(epoch_losses)), epoch_losses, label="Epoch losses")
-    plt.plot(val_epochs, ca_values, label="CA on validation set")
-    plt.xticks(range(0, len(epoch_losses)+1, 5))
-    plt.legend()
+
+    fig1, ax1 = plt.subplots()
+    fig1.set_size_inches(10, 7)
+    ax1.plot(range(len(epoch_losses)), epoch_losses)
+    ax1.set_xticks(range(0, len(epoch_losses)+1, 5))
+    fig1.suptitle("Epoch losses")
+
+    fig2, ax2 = plt.subplots()
+    fig2.set_size_inches(10, 7)
+    ax2.plot(val_epochs, ca_values)
+    ax2.set_xticks(range(0, len(epoch_losses)+1, 5))
+    fig2.suptitle("CA on validation set")
+
     plt.show()
--- HW1/utils.py
+++ HW1/utils.py
@@ -14,12 +14,41 @@ def softmax_dLdZ(output, target):
     return output - target
 
 
-def cross_entropy(y_true, y_pred, epsilon=1e-12):
+def cross_entropy(y_true, y_pred, epsilon=1e-12, lmbd=0.0, n=None, weights=None):
+    """
+    Calculate cross entropy, given predictions and ground truths.
+
+    Parameters:
+        y_true  - ground truths i.e. targets for the provided predictions
+
+        y_pred  - predictions, i.e. the output of the neural network
+
+        epsilon - small constant for numerical stability
+
+        lmbd    - L2 regularization parameter
+
+        n       - size of the TODO, used when calculating the regularization term
+
+        weights - weights of the neural network
+    """
     targets = y_true.transpose()
     predictions = y_pred.transpose()
     predictions = np.clip(predictions, epsilon, 1. - epsilon)
-    N = predictions.shape[0]
-    ce = -np.sum(targets * np.log(predictions + 1e-9)) / N
+    num_samples = predictions.shape[0]
+    ce = -np.sum(targets * np.log(predictions + 1e-9)) / num_samples
+
+    if lmbd != 0.0:
+        if not weights:
+            raise ValueError("No weights provided for calculating L2 regularization term.")
+        if not n:
+            raise ValueError("Size of training data (n) not given for calculating L2 regularization term.")
+
+        # L2 regularization
+        sum = 0
+        for layer_weights in weights:
+            sum += np.sum(np.square(layer_weights))
+        reg_term = (lmbd / 2*n) * sum
+        ce += reg_term
     return ce
 
 
@@ -34,3 +63,21 @@ def sigmoid_prime(z):
 def unpickle(file):
     with open(file, 'rb') as fo:
         return pickle.load(fo, encoding='bytes')
+    
+
+def load_data_cifar(train_file, test_file):
+    """
+    Load the CIFAR10 dataset
+    """
+    train_dict = unpickle(train_file)
+    test_dict = unpickle(test_file)
+    train_data = np.array(train_dict['data']) / 255.0
+    train_class = np.array(train_dict['labels'])
+    train_class_one_hot = np.zeros((train_data.shape[0], 10))
+    train_class_one_hot[np.arange(train_class.shape[0]), train_class] = 1.0
+    test_data = np.array(test_dict['data']) / 255.0
+    test_class = np.array(test_dict['labels'])
+    test_class_one_hot = np.zeros((test_class.shape[0], 10))
+    test_class_one_hot[np.arange(test_class.shape[0]), test_class] = 1.0
+    
+    return train_data.transpose(), train_class_one_hot.transpose(), test_data.transpose(), test_class_one_hot.transpose()
